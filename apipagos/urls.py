9c53ab020310113cf98691642788543df23d8698
CristianD <crisdic5@gmail.com>
2022-12-20 16:36:56 -0500

    Actualizacion apipagos y pagos

--- apipagos/urls.py
+++ apipagos/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include, re_path
-# Nuevos imports añadidos
+
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -34,7 +34,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('', include('pagos.urls')),
     re_path(r'api/redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
