514bd8a7de9182376015e5e3e9918cf2b673ba31
CristianD <crisdic5@gmail.com>
2022-12-26 01:42:15 -0500

    Actualizacion serializer para desarrollo frontend

--- users/serializers.py
+++ users/serializers.py
@@ -35,4 +35,4 @@ class GetUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["email", "username", "password"] 
+        fields = ["id","email", "username", "password"] 
